8e912cfe32ec816411c1f93f0c2e063f618932d1
Arezlon <50326537+Arezlon@users.noreply.github.com>
2021-04-25 03:26:55 -0300

    #4 Perfil del usuario v2

--- app/src/main/java/com/grupo4/inmobiliaria/ui/ui/perfil/PerfilFragment.java
+++ app/src/main/java/com/grupo4/inmobiliaria/ui/ui/perfil/PerfilFragment.java
@@ -7,22 +7,50 @@ import android.view.ViewGroup;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
 import com.grupo4.inmobiliaria.R;
+import com.grupo4.inmobiliaria.modelo.Propietario;
+
 
 public class PerfilFragment extends Fragment {
 
     private PerfilViewModel perfilViewModel;
+    public TextView tvUserId;
+    public TextView tvUserFullname;
+    public TextView tvUserDni;
+    public TextView tvUserEmail;
+    public TextView tvUserPhone;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         perfilViewModel =
                 new ViewModelProvider(this).get(PerfilViewModel.class);
         View root = inflater.inflate(R.layout.fragment_perfil, container, false);
+
+        InicializarVista(root);
+        perfilViewModel.getPropietarioMutable().observe(getViewLifecycleOwner(), new Observer<Propietario>() {
+            @Override
+            public void onChanged(Propietario propietario) {
+                tvUserId.setText("Usuario #"+propietario.getId());
+                tvUserFullname.setText(propietario.getApellido()+" "+propietario.getNombre());
+                tvUserDni.setText("DNI: "+propietario.getDni());
+                tvUserEmail.setText("E-Mail: "+propietario.getEmail());
+                tvUserPhone.setText("Tel: "+propietario.getTelefono());
+            }
+        });
+        perfilViewModel.LeerPropietario();
+
         return root;
+    }
+
+    private void InicializarVista(View v){
+        tvUserId = v.findViewById(R.id.tvUserId);
+        tvUserFullname = v.findViewById(R.id.tvUserFullname);
+        tvUserDni = v.findViewById(R.id.tvUserDni);
+        tvUserEmail = v.findViewById(R.id.tvUserEmail);
+        tvUserPhone = v.findViewById(R.id.tvUserPhone);
     }
 }
--- app/src/main/java/com/grupo4/inmobiliaria/ui/ui/perfil/PerfilViewModel.java
+++ app/src/main/java/com/grupo4/inmobiliaria/ui/ui/perfil/PerfilViewModel.java
@@ -4,16 +4,26 @@ import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
+import com.grupo4.inmobiliaria.modelo.Inmueble;
+import com.grupo4.inmobiliaria.modelo.Propietario;
+import com.grupo4.inmobiliaria.request.ApiClient;
+
+import java.util.ArrayList;
+
 public class PerfilViewModel extends ViewModel {
+    public MutableLiveData<Propietario> propietarioMutable;
 
-    private MutableLiveData<String> mText;
-
-    public PerfilViewModel() {
-        mText = new MutableLiveData<>();
-        mText.setValue("Perfil");
+    public LiveData<Propietario> getPropietarioMutable(){
+        if (propietarioMutable == null){
+            propietarioMutable = new MutableLiveData<>();
+        }
+        return propietarioMutable;
     }
 
-    public LiveData<String> getText() {
-        return mText;
+    public void LeerPropietario(){
+        ApiClient api = ApiClient.getApi();
+        Propietario p = api.obtenerUsuarioActual();
+
+        propietarioMutable.setValue(p);
     }
 }
