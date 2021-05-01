61af9f5ac30dc19e6375c4c6b30495c59aa0a261
Arezlon <50326537+Arezlon@users.noreply.github.com>
2021-05-01 16:19:56 -0300

    #4 Validación al editar el perfil

--- app/src/main/java/com/grupo4/inmobiliaria/ui/ui/perfil/EditarPerfilViewModel.java
+++ app/src/main/java/com/grupo4/inmobiliaria/ui/ui/perfil/EditarPerfilViewModel.java
@@ -1,5 +1,7 @@
 package com.grupo4.inmobiliaria.ui.ui.perfil;
 
+import android.util.Patterns;
+
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
@@ -34,12 +36,19 @@ public class EditarPerfilViewModel extends ViewModel {
 
     public void ModificarPropietario(Propietario p){
         if(p.getNombre().isEmpty() || p.getApellido().isEmpty() || p.getDni() == -1 || p.getEmail().isEmpty() || p.getTelefono().isEmpty()){
-            errorMutable.setValue("Error, los campos no pueden estar vacios.");
+            errorMutable.setValue("Los campos no pueden estar vacios.");
+        }else if(p.getDni().toString().length() != 8){
+            errorMutable.setValue("El DNI ingresado no es válido (8 dígitos necesarios)");
+        }else if(p.getNombre().length() > 16 || p.getNombre().length() < 3 || p.getApellido().length() > 16 || p.getApellido().length() < 3){
+            errorMutable.setValue("El nombre/apellido ingresado no es válido (3 caracteres mínimo, 16 máximo)");
+        }else if(!Patterns.EMAIL_ADDRESS.matcher(p.getEmail()).matches()){
+            errorMutable.setValue("La dirección de correo electrónico ingresada no es válida.");
+        }else if(p.getTelefono().length() > 15 || p.getTelefono().length() < 10){
+            errorMutable.setValue("El número de teléfono ingresado no es válido (10-15 dígitos)");
         }else{
             ApiClient api = ApiClient.getApi();
             api.actualizarPerfil(p);
             errorMutable.setValue("EXITO");
         }
-
     }
 }
