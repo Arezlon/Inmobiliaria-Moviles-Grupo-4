b1fd9c27d55d355946b10924e450b908dbe31d82
Arezlon <50326537+Arezlon@users.noreply.github.com>
2021-05-02 16:58:48 -0300

    #6 y #8 Lista de pagos

--- app/src/main/java/com/grupo4/inmobiliaria/ui/ui/contratos/ListaPagosAdapter.java
+++ app/src/main/java/com/grupo4/inmobiliaria/ui/ui/contratos/ListaPagosAdapter.java
@@ -1,23 +1,16 @@
 package com.grupo4.inmobiliaria.ui.ui.contratos;
 
-import android.app.Activity;
 import android.content.Context;
-import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
-import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-import androidx.navigation.Navigation;
 
-import com.bumptech.glide.Glide;
-import com.bumptech.glide.load.engine.DiskCacheStrategy;
 import com.grupo4.inmobiliaria.R;
-import com.grupo4.inmobiliaria.modelo.Inmueble;
 import com.grupo4.inmobiliaria.modelo.Pago;
 
 import java.util.List;
@@ -40,9 +33,15 @@ public class ListaPagosAdapter extends ArrayAdapter<Pago> {
         convertView = convertView != null ? convertView : inflater.inflate(R.layout.list_item_pago, parent, false);
         Pago pago = pagos.get(position);
 
-        TextView tvPago = convertView.findViewById(R.id.tvPago);
+        TextView tvPagoId = convertView.findViewById(R.id.tvPagoId);
+        TextView tvPagoFecha = convertView.findViewById(R.id.tvPagoFecha);
+        TextView tvPagoNumero = convertView.findViewById(R.id.tvPagoNumero);
+        TextView tvPagoImporte = convertView.findViewById(R.id.tvPagoImporte);
 
-        tvPago.setText(String.valueOf(pago.getImporte()));
+        tvPagoId.setText("#" + String.valueOf(pago.getIdPago()));
+        tvPagoFecha.setText(pago.getFechaDePago());
+        tvPagoNumero.setText("Nro de pago: " + String.valueOf(pago.getNumero()));
+        tvPagoImporte.setText("$" + String.valueOf(pago.getImporte()));
 
         return convertView;
     }
